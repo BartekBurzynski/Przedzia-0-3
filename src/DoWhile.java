2cc7172d6bf9f02b67b3475e53f1ce2396f0df20
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-05-22 21:40:12 +0200

    Stworzenie przedziału od 0-3

---
+++ src/DoWhile.java
@@ -0,0 +1,12 @@
+public class DoWhile {
+    public static void main(String[] args) {
+         {
+            double i =  0;
+            do {
+                System.out.println(i);
+                i = i + 0.1;
+            }while(i <= 3);
+
+        }
+    }
+}
---
+++ src/Petla.java
@@ -0,0 +1,14 @@
+public class Petla {
+    public static void main(String[] args) {
+        double i = 0;
+        while (i <= 3){
+            System.out.println(i);
+            i = i + 0.1;
+        }
+
+
+
+
+
+    }
+}
